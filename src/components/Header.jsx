26e787659978232499129f4dd1b54353a5f2bfca
chazmcgrill <charlietaylordev@gmail.com>
2018-01-22 17:46:25 +0000

    Nav Logic
    Added logic to toggle nav on hamburger click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,26 +3,23 @@ import React, { Component } from 'react'
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      hamburgerOpen: false
-    }
+    this.state = { hamburgerOpen: false }
     this.hamburgerClick = this.hamburgerClick.bind(this);
   }
 
-
   hamburgerClick() {
     this.setState({hamburgerOpen: !this.state.hamburgerOpen});
-    
-    // nav.classList.toggle('hidden');
   }
 
   render() {
-    let hamburgerClass = `hamburger hamburger--spin ham-handle ${this.state.hamburgerOpen ? 'is-active' : ''}`;
+    const {hamburgerOpen} = this.state;
+    let hamburgerClass = `hamburger hamburger--spin ham-handle ${hamburgerOpen ? 'is-active' : ''}`;
+    let navClass = this.props.width < 640 && !hamburgerOpen ? "hidden" : "";
 
     return (
       <header>
         <img src="assets/img/hclogo.png" alt="Hurricane Charlie Logo" />
-        <nav className={this.props.width > 639 ? "" : "hidden"}>
+        <nav className={navClass}>
           <li><a href="#">gallery</a></li>
           <li><a href="#">contact</a></li>
           <li><a href="#">shop</a></li>
