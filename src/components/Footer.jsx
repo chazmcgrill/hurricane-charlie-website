6334c1dd297d6417d18b4a6c0101d0c3e16aa032
chazmcgrill <charlietaylordev@gmail.com>
2018-01-22 17:56:11 +0000

    Added Footer Markup and Styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,19 @@ import React, { Component } from 'react'
 class Footer extends Component {
   render() {
     return (
-      <h1>Footer</h1>
+      <footer>
+        <nav>
+          <li><a href="#">gallery</a></li>
+          <li><a href="#">contact</a></li>
+          <li><a href="#">shop</a></li>
+        </nav>
+        <div className="footer-icons">
+          <a href="http://twitter.com/hurricanechaz"><i className="fa fa-twitter"></i></a>
+          <a href="http://www.instagram.com/chazhurricane"><i className="fa fa-instagram"></i></a>
+          <a href="http://www.behance.net/chazhurricane"><i className="fa fa-behance"></i></a>
+        </div>
+        <p>&copy; 2018 Charlie Taylor all rights reserved.</p>
+      </footer>
     )
   }
 }
