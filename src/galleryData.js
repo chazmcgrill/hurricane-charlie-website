3b6e4605367894d2330e5c7278bb0859980bc277
chazmcgrill <charlietaylordev@gmail.com>
2018-01-31 17:20:50 +0000

    Add Gallery Data Object

---
+++ src/galleryData.js
@@ -0,0 +1,5 @@
+const galleryData = [
+  { id: 0, src: "assets/img/gallery1.jpg", size: "small" },
+  { id: 1, src: "assets/img/gallery1.jpg", size: "small" },
+  { id: 2, src: "assets/img/gallery1.jpg", size: "small" }
+];
