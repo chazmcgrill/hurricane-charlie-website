dd7e11877b48655b21de0a04bd7d48f40adc508b
chazmcgrill <charlietaylordev@gmail.com>
2018-04-30 10:05:37 +0100

    Add Thumbnail Images

--- assets/sql/gallery.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE hurricane_charlie;
-
-CREATE TABLE gallery (
-  id INT DEFAULT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  img_name VARCHAR(30),
-  img_url VARCHAR(100),
-  info TEXT(500)
-)
