690c576784cd6f94d6a154c2927be560979c9d34
chazmcgrill <charlietaylordev@gmail.com>
2018-05-01 17:10:39 +0100

    Created Mailer CTA on Shop

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div>
+      <h1>Shop</h1>
+      <div className="mailer-cta">
+        <h2>Be the first to know about new items</h2>
+        <div className="mailer-form">
+          <input type="email" placeholder="your email" />
+          <button>join</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Shop;
