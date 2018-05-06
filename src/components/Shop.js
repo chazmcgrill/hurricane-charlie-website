d3760c3172af0e534ad4c3713a773d9998e9a405
chazmcgrill <charlietaylordev@gmail.com>
2018-05-06 11:35:24 +0100

    Added Mailing List Message Logic

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-function emailErrorCheck(email) {
+function validateEmail(email) {
   const emailRegx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (email.length === 0) {
     return "email is required";
@@ -13,16 +13,18 @@ function emailErrorCheck(email) {
 class Shop extends Component {
   constructor() {
     super();
-    this.state = { email: '', status: null };
+    this.state = { email: '', msg: null };
     this.mailListChange = this.mailListChange.bind(this);
     this.mailListSubmit = this.mailListSubmit.bind(this);
   }
 
   mailListSubmit() {
-    // email validation
-    let status = emailErrorCheck(this.state.email);
-    console.log(status);
+    let msg = validateEmail(this.state.email);
     // post to database
+    if (!msg) {
+      console.log("sending to db");
+    }
+    this.setState({msg})
   }
 
   mailListChange(e) {
@@ -37,6 +39,7 @@ class Shop extends Component {
         <div className="mailer-cta">
           <h2>Be the first to know about new items</h2>
           <div className="mailer-form">
+            { this.state.msg ? <p>{this.state.msg}</p> : null }
             <input 
               value={this.state.email} 
               onChange={this.mailListChange} 
