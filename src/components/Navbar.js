59e9467325a00ea10ea21a2ca91ddf590d45a1f4
chazmcgrill <charlietaylordev@gmail.com>
2018-05-08 16:14:44 +0100

    Added Active Button Logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 class Navbar extends Component {
   render() {
-    const nav = this.props.navData.map(item => (
-      <li id={item.name} key={item.id} onClick={this.props.navClick}>{item.name}</li>
-    ));
+    const nav = this.props.navData.map(item => {
+      const border = item.active ? "3px solid coral" : "none";
+      return (
+        <li 
+          id={item.name} key={item.id} 
+          onClick={this.props.navClick} 
+          style={{ borderBottom: border }} 
+        >{item.name}</li>
+      );
+    });
   
     return(
       <nav>
