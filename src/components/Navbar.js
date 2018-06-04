b01767597496ef7e3782a062ab23fe9d51afc4cb
chazmcgrill <charlietaylordev@gmail.com>
2018-06-04 18:27:52 +0100

    Refactored Navbar Made Stateless

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,18 @@ import { NavLink } from 'react-router-dom';
 
 const activeNav = { borderBottom: "3px solid #FF2E63" };
 
-class Navbar extends Component {
-  render() {
-    const { burgerOpen, navData } = this.props;
-    const nav = navData.map(item => (
-      <li key={item.id}>
-        <NavLink to={item.url} exact activeStyle={activeNav}>
-          {item.name}
-        </NavLink>
-      </li>
-    ));
-  
-    return(
-      <nav>
-        { nav }
-      </nav>
-    )
-  }
+const Navbar = ({ burgerOpen, navData }) => {
+  return (
+    <nav>
+      {navData.map(item => (
+        <li key={item.id}>
+          <NavLink to={item.url} exact activeStyle={activeNav}>
+            {item.name}
+          </NavLink>
+        </li>
+      ))}
+    </nav>
+  )
 }
 
 export default Navbar;
