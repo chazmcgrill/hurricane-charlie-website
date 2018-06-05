0a0d6eab15638eeae42d7ff30c0b653cf7101dcf
chazmcgrill <charlietaylordev@gmail.com>
2018-06-05 14:06:57 +0100

    Converted Footer to Stateless Comp'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,18 @@ import React, { Component } from 'react';
 import Navbar from './Navbar';
 import socialItems from '../globals/socialItems';
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer>
-        <Navbar navClick={this.props.navClick} navData={this.props.navData} />
-        <div className="footer-icons">
-          {socialItems.map(s => (
-            <a key={s.id} target="_blank" href={s.url}><i className={`fa fa-${s.name}`}></i></a>
-          ))}
-        </div>
-        <p>&copy; 2018 Charlie Taylor all rights reserved.</p>
-      </footer>
-    )
-  }
+const Footer = ({ navData }) => {
+  return (
+    <footer>
+      <Navbar navData={navData} />
+      <div className="footer-icons">
+        {socialItems.map(s => (
+          <a key={s.id} target="_blank" href={s.url}><i className={`fa fa-${s.name}`}></i></a>
+        ))}
+      </div>
+      <p>&copy; 2018 Charlie Taylor all rights reserved.</p>
+    </footer>
+  )
 }
 
 export default Footer
