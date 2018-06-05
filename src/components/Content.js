de89ba26b04c39e12ff488307325f9cf41a3ed7c
chazmcgrill <charlietaylordev@gmail.com>
2018-06-05 09:39:01 +0100

    Created A Redirect from Root to Gallery

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch  } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Gallery from './Gallery';
 import Contact from './Contact';
 import Shop from './Shop';
@@ -8,10 +8,10 @@ const Content = ({ galleryData }) => {
   return (
     <section className="content-section">
       <Switch>
+        <Redirect from="/" to="/gallery" exact />
         <Route
-          path="/"
+          path="/gallery"
           render={() => <Gallery galleryData={galleryData} />}
-          exact
         />
         <Route path="/contact" component={Contact} />
         <Route path="/shop" component={Shop} />
