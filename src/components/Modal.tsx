602794a8fa23eb07b3e4002dc3f41800ba3ddafe
chazmcgrill <charlietaylorcoder@gmail.com>
2020-03-22 17:45:06 +0000

    Adds modal icons

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Img from "gatsby-image";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faArrowAltCircleLeft, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import { IGalleryItem } from '../pages/gallery';
 
 interface ModalProps {
@@ -34,13 +35,15 @@ const Modal = ({
         
                 <div className="modal-btns">
                     <div onClick={() => modalHandler('prev')}>
-                        <i className={`fas fa-arrow-alt-circle-left ${id >= 1 ? null : "inactive"}`}/>
+                        <FontAwesomeIcon icon={faArrowAltCircleLeft} style={{ color: id >= 1 ? '#000' : 'lightgrey' }} />
                     </div>
 
-                    <div onClick={() => modalHandler('close')}><i className="fas fa-times"/></div>
+                    <div onClick={() => modalHandler('close')}>
+                        <FontAwesomeIcon icon={faTimes} style={{ color: '#000000' }} />
+                    </div>
           
                     <div onClick={() => modalHandler('next')}>
-                        <i className={`fas fa-arrow-alt-circle-right ${id < modalLimit ? null : "inactive"}`}/>
+                        <FontAwesomeIcon icon={faArrowAltCircleRight} style={{ color: id < modalLimit ? '#000' : 'lightgrey' }} />
                     </div>
                 </div>
             </div>
