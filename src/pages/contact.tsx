0db001a9499ff32a9e58101d4f539eb1adb0aa2f
chazmcgrill <charlietaylorcoder@gmail.com>
2020-03-24 19:36:34 +0000

    Removal of class components and refactor

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,45 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CallToAction from '../components/call-to-action';
-import { formValidator, FormValidatorData } from '../helpers/validators';
+import { formValidator } from '../helpers/validators';
 import Layout from '../components/layout';
 import SocialIcons from '../components/social-icons';
 import SEO from '../components/seo';
 
-interface ContactState {
-    data: FormValidatorData;
-    errorMessages: FormValidatorData;
-    msgStatus: MessageStatus;
-}
+const DEFAULT_MESSAGE_STATE = { name: '', email: '', message: '' };
 
-interface MessageStatus {
-    msg: string;
-    status: string;
-}
+const Contact = () => {
+    const [data, setData] = useState(DEFAULT_MESSAGE_STATE);
+    const [errorMessages, setErrorMessages] = useState(DEFAULT_MESSAGE_STATE);
+    const [msgStatus, setMsgStatus] = useState({ msg: '', status: '' });
 
-class Contact extends Component<{}, ContactState> {
-    state = {
-        data: { name: '', email: '', message: '' },
-        errorMessages: { name: '', email: '', message: '' },
-        msgStatus: { msg: '', status: '' },
-    };
-
-    handleInput = (e: React.FormEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
-        this.setState({ 
-            data: { 
-                ...this.state.data, 
-                [e.currentTarget.name]: e.currentTarget.value, 
-            }, 
-        });
+    const handleInput = (e: React.FormEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+        setData({ ...data, [e.currentTarget.name]: e.currentTarget.value });
     }
 
-    handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
-        const { errorMessages, isValid } = formValidator(this.state.data);
+        const { errorMessages, isValid } = formValidator(data);
 
         if (isValid) {
-            const { data } = this.state;
-            let { msgStatus } = this.state;
-      
+            let newMessageStatus = { ...msgStatus };
+
             try {
                 const { ok } = await fetch('https://ct-core-api.herokuapp.com/hc-contact', {
                     method: 'POST',
@@ -50,80 +33,82 @@ class Contact extends Component<{}, ContactState> {
                 });
 
                 if (ok) {
-                    msgStatus = { msg: "message sent, speak to you soon...", status: "green" };
+                    newMessageStatus = {
+                        msg: 'message sent, speak to you soon...',
+                        status: 'green',
+                    };
                 }
             } catch (err) {
-                msgStatus = { msg: "message failed! please retry.", status: "red" };
+                newMessageStatus = {
+                    msg: 'message failed! please retry.',
+                    status: 'red',
+                };
             }
 
-            this.setState({
-                data: { name: '', email: '', message: '' },
-                errorMessages,
-                msgStatus,
-            });
-  
-        } else {
-            this.setState({ errorMessages });
+            setData(DEFAULT_MESSAGE_STATE);
+            setMsgStatus(newMessageStatus);
         }
+        
+        setErrorMessages(errorMessages);
     }
 
-    render() {
-        const { name, email, message } = this.state.data;
-        const { errorMessages, msgStatus } = this.state;
+    const { name, email, message } = data;
 
-        return (
-            <Layout>
-                <SEO title="404: Not found" />
-                <div className="contact-box">
-                    <div className="contact-item">
-                        <h2>say hello...</h2>
-                        <form>
-                            {msgStatus.msg ? <p style={{ color: msgStatus.status }}>{ msgStatus.msg }</p> : null}
-                            {errorMessages.name ? <p className="err-msg">{errorMessages.name}</p> : null}
-                            <input 
-                                name="name"
-                                value={name} 
-                                placeholder="your name" 
-                                onChange={this.handleInput} 
-                            />
-                            {errorMessages.email ? <p className="err-msg">{errorMessages.email}</p> : null}
-                            <input 
-                                name="email"
-                                value={email} 
-                                placeholder="your email" 
-                                onChange={this.handleInput} 
-                            />
-                            {errorMessages.message ? <p className="err-msg">{errorMessages.message}</p> : null}
-                            <textarea 
-                                name="message"
-                                value={message}
-                                cols={30} rows={10} 
-                                placeholder="type your message here..." 
-                                onChange={this.handleInput} 
-                            />
-                            <button
-                                onClick={this.handleSubmit}
-                            >send</button>
-                        </form>
-                    </div>
+    return (
+        <Layout>
+            <SEO title="404: Not found" />
+            <div className="contact-box">
+                <div className="contact-item">
+                    <h2>say hello...</h2>
+                    <form>
+                        {msgStatus.msg ? <p style={{ color: msgStatus.status }}>{msgStatus.msg}</p> : null}
 
-                    <div className="contact-item contact-text">
-                        <div>
-                            <p>{`Please feel free to contact me with any questions. I'm open to commissions and exhibitions so do get in touch.`}</p>
-                            <h2>social</h2>
-                            <p>You can find me on these social platforms:</p>
-                            <div className="contact-icons">
-                                <SocialIcons />
-                            </div>
-                            <h2>coding</h2>
-                            <p>I designed and coded this website. For more information please visit my web development portfolio <a target="_blank" rel="noopener noreferrer" href="http://charlietaylorcoder.com">here</a></p>
+                        {errorMessages.name ? <p className="err-msg">{errorMessages.name}</p> : null}
+                        <input
+                            name="name"
+                            value={name}
+                            placeholder="your name"
+                            onChange={handleInput}
+                        />
+
+                        {errorMessages.email ? <p className="err-msg">{errorMessages.email}</p> : null}
+                        <input
+                            name="email"
+                            value={email}
+                            placeholder="your email"
+                            onChange={handleInput}
+                        />
+
+                        {errorMessages.message ? <p className="err-msg">{errorMessages.message}</p> : null}
+                        <textarea
+                            name="message"
+                            value={message}
+                            cols={30} rows={10}
+                            placeholder="type your message here..."
+                            onChange={handleInput}
+                        />
+
+                        <button onClick={handleSubmit}>send</button>
+                    </form>
+                </div>
+
+                <div className="contact-item contact-text">
+                    <div>
+                        <p>{`Please feel free to contact me with any questions. I'm open to commissions and exhibitions so do get in touch.`}</p>
+                        <h2>social</h2>
+                        <p>You can find me on these social platforms:</p>
+                        <div className="contact-icons">
+                            <SocialIcons />
                         </div>
+                        <h2>coding</h2>
+                        <p>I designed and coded this website. For more information please visit my web development portfolio <a target="_blank" rel="noopener noreferrer" href="http://charlietaylorcoder.com">here</a></p>
                     </div>
                 </div>
-                <CallToAction />
-            </Layout>
-        );
-    }
+            </div>
+        
+            <CallToAction />
+        </Layout>
+    );
 }
 
 export default Contact;
--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -1,16 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-
 import ShopItem, { ShopItemData } from '../components/shop-item';
-import { validateEmail } from '../helpers/validators';
 import Layout from '../components/layout';
 import { imageObjectFromArray } from '../helpers/imageObjectFromArray';
 import SEO from '../components/seo';
+import MailingListForm from '../components/mailing-list-form';
 
-const Shop = () => {
-    const [email, setEmail] = useState('');
-    const [message, setMsg] = useState('');
-
+const Shop = (): JSX.Element => {
     const { images, data } = useStaticQuery(graphql`
         query {
             images: allFile(filter: { relativeDirectory: { eq: "shop" } }) {
@@ -39,36 +35,10 @@ const Shop = () => {
 
     const flattendImageData = imageObjectFromArray(images.nodes);
 
-    const mailListSubmit = async (): Promise<void> => {
-        const isEmailValid = validateEmail(email);
-        let newMessage = '';
-
-        if (!isEmailValid) {
-            try {
-                const messageRequest = await fetch('https://ct-core-api.herokuapp.com/hc-mailer' , {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ email }),
-                });
-
-                if (messageRequest.ok) {
-                    newMessage = 'You\'ve been added to the mailing list';
-                }
-            } catch (error) {
-                newMessage = 'Something went wrong, please try again.';
-            }
-        }
-
-        setMsg(newMessage);
-    }
-
-    const mailListChange = (e: React.FormEvent<HTMLInputElement>): void => setEmail(e.currentTarget.value);
-  
-    const statusMsg = message && <p>{message}</p>;
-
     return (
         <Layout>
             <SEO title="Shop" />
+
             <div className="shop-container">
                 {data.shop.map((shopItem: ShopItemData) => (
                     <ShopItem
@@ -79,18 +49,7 @@ const Shop = () => {
                 ))}
             </div>
 
-            <div className="mailer-cta">
-                <h2>Be the first to know about new items</h2>
-                {statusMsg}
-                <div className="mailer-form">
-                    <input
-                        value={email}
-                        onChange={mailListChange}
-                        type="email" placeholder="your email"
-                    />
-                    <button onClick={mailListSubmit}>join</button>
-                </div>
-            </div>
+            <MailingListForm />
         </Layout>
     );
 }
