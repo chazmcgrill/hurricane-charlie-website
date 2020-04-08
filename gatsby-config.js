2c11fd3fbf0925cfec975742b797b12ed67f56d6
chazmcgrill <charlietaylorcoder@gmail.com>
2020-04-08 08:57:56 +0100

    Fixes site icon aspect ratio

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/hclogo.png`, // This path is relative to the root of the site.
+                icon: `src/images/hcicon.png`, // This path is relative to the root of the site.
             },
         },
     // this (optional) plugin enables Progressive Web App + Offline functionality
