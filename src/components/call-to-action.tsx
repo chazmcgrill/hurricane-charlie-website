b7cc91f40d6a68c4f2e60eb146df191b97208f33
Charlie Taylor <charlietaylorcoder@gmail.com>
2020-04-08 17:54:32 +0100

    Release@v2.6.0 (#19)
    * 2.6.0
    
    * Revert "Fixes max resolution for modal images"
    
    This reverts commit 1e12b69921aa7520de41857c9e78990cd590b68a.
    
    * Fixes modal images size
    
    * Revert "Revert "Gallery Grid Improvements""
    
    This reverts commit ec9f42a834dca48bba9d2eeea67acb7c1ede8da7.
    
    * Revert "Revert "Revert "Gallery Grid Improvements"""
    
    This reverts commit ba7a5c7f8ffc534b707c8ce3730ef9ac5189eae4.
    
    * Revert "Merge branch 'master-copy' into release@v2.6.0"
    
    This reverts commit e45dc15e8590bae7c3deae3b30dee5d01d24b4d0, reversing
    changes made to dd8248d5aa34b8997d6da02753144f3aa9be457f.

--- src/components/call-to-action.tsx
+++ src/components/call-to-action.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import BackgroundImage from 'gatsby-background-image';
 
-const CallToAction = (): JSX.Element => (
-    <div className="shop-cta">
-        <div className="shop-cta-overlay"></div>
-        <div className="shop-cta-box">
-            <h2>Prints for your wall...</h2>
-            <Link to="/shop">visit shop</Link>
-        </div>
-    </div>
-);
+const CallToAction = (): JSX.Element => {
+    const { heroImage } = useStaticQuery(graphql`
+        query {
+            heroImage: file(relativePath: {eq: "hero.jpg"}) {
+                childImageSharp {
+                    fluid {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `);
+
+    return (
+        <BackgroundImage
+            fluid={heroImage.childImageSharp.fluid}
+            style={{ backgroundPosition: 'top center' }}
+            className="shop-cta"
+        >
+            <div className="shop-cta-overlay"></div>
+            <div className="shop-cta-box">
+                <h2>Prints for your wall...</h2>
+                <Link to="/shop">visit shop</Link>
+            </div>
+        </BackgroundImage>
+    );
+}
 
 export default CallToAction;
--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import ShopItem, { ShopItemData } from '../components/shop-item';
 import Layout from '../components/layout';
-import { fluidImageObjectFromArray } from '../helpers/imageObjectFromArray';
+import { fluidImageObjectFromArray, GatsbyFluidImageNode } from '../helpers/imageObjectFromArray';
 import SEO from '../components/seo';
+
+const combineDataAndImages = (images: GatsbyFluidImageNode[], data: ShopItemData[]) => {
+    const flattendImageData = fluidImageObjectFromArray(images);
+    return data.map((item) => ({
+        ...item,
+        image: flattendImageData[item.url],
+    }));
+}
 
 const Shop = (): JSX.Element => {
     const { images, data } = useStaticQuery(graphql`
@@ -32,19 +40,14 @@ const Shop = (): JSX.Element => {
         }
     `);
 
-    const flattendImageData = fluidImageObjectFromArray(images.nodes);
+    const shopItems = combineDataAndImages(images.nodes, data.shop);
 
     return (
         <Layout>
             <SEO title="Shop" />
-
             <div className="shop-container">
-                {data.shop.map((shopItem: ShopItemData) => (
-                    <ShopItem
-                        key={shopItem.id}
-                        product={shopItem}
-                        imgData={flattendImageData[shopItem.url]}
-                    />
+                {shopItems.map((shopItem: ShopItemData) => (
+                    <ShopItem key={shopItem.id} product={shopItem} />
                 ))}
             </div>
         </Layout>
