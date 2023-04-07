065231036d016362f38d064eaa42d7857ca6cf95
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2023-04-07 13:45:08 +0100

    feat: replace react spring with framer motion for modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { animated, useTransition } from 'react-spring';
+import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { ChildrenType } from './layout';
 
 interface ModalProps {
@@ -9,41 +9,49 @@ interface ModalProps {
 }
 
 const Modal = ({ children, isModalOpen, onOutsideClick }: ModalProps): any => {
-    const [show, setShow] = useState(false);
+    // const [show, setShow] = useState(false);
 
-    const modalTransitions = useTransition(show, null, {
-        from: { transform: 'translateY(100px)' },
-        enter: { transform: 'translateY(0px)' },
-        leave: { transform: 'translateY(100px)' },
-    });
+    // const [modalTransitions] = useTransition(show, () => ({
+    //     from: { transform: 'translateY(100px)' },
+    //     enter: { transform: 'translateY(0px)' },
+    //     leave: { transform: 'translateY(100px)' },
+    // }));
 
-    const backgroundTransitions = useTransition(show, null, {
-        from: { opacity: 0 },
-        enter: { opacity: 1 },
-        leave: { opacity: 0 },
-    });
+    // const [backgroundTransitions] = useTransition(show, () => ({
+    //     from: { opacity: 0 },
+    //     enter: { opacity: 1 },
+    //     leave: { opacity: 0 },
+    // }));
 
-    const handleModalClick = (e: React.MouseEvent<HTMLElement>) => e.stopPropagation();
+    // const handleModalClick = (e: React.MouseEvent<HTMLElement>) => e.stopPropagation();
 
-    useEffect(() => {
-        document.body.style.overflow = isModalOpen ? 'hidden' : 'auto';
-        setShow(Boolean(isModalOpen));
-    }, [isModalOpen]);
+    // useEffect(() => {
+    //     document.body.style.overflow = isModalOpen ? 'hidden' : 'auto';
+    //     setShow(Boolean(isModalOpen));
+    // }, [isModalOpen]);
 
-    return backgroundTransitions.map(
-        ({ item, key, props }) =>
-            item && (
-                <animated.div className="modal-background" style={props} key={key} onClick={onOutsideClick}>
-                    {modalTransitions.map(
-                        ({ item, key, props }) =>
-                            item && (
-                                <animated.div className="modal" style={props} key={key} onClick={handleModalClick}>
-                                    {children && children}
-                                </animated.div>
-                            ),
-                    )}
-                </animated.div>
-            ),
+    return (
+        <AnimatePresence>
+            {isModalOpen && !!children && (
+                <motion.div
+                    className="modal-background"
+                    onClick={onOutsideClick}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                >
+                    <motion.div
+                        className="modal"
+                        // onClick={handleModalClick}
+                        initial={{ transform: 'translateY(100px)' }}
+                        animate={{ transform: 'translateY(0px)' }}
+                        exit={{ transform: 'translateY(100px)' }}
+                    >
+                        {children}
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>
     );
 };
 
