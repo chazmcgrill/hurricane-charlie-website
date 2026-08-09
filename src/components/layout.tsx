119bbcaffd4bb521c3decbbe7e54ddf846cd1794
Charlie Taylor <mail@charlietaylorcoder.com>
2026-08-09 23:07:04 +0100

    fix: upgrades nextjs to fix vulnerabilities (#24)
    * fix: upgrades nextjs to fix vulnerabilities
    
    * fix: pegs node version to 24 for vercal compat
    
    * fix: audit fixes
    
    * fix: upgrades nodemailer to fix vulnerability

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, Fragment, type JSX } from 'react';
 import Header from '@components/header';
 import Footer from '@components/footer';
 import Modal from '@components/modal';
